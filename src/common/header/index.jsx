bb9d24f11c4dac390f1fd035096021ebdc10e9e2
yash-babariya <yashbabariya549@gmail.com>
2024-08-23 11:12:52 +0530

    some changes

--- src/common/header/index.jsx
+++ src/common/header/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './header.scss';
 import logo from '../../assets/logo/logo.webp'
 import { PiHeartStraight } from "react-icons/pi";
@@ -8,12 +8,47 @@ import { SlUser } from "react-icons/sl";
 import categoryApi from '../../categoryApi/categoryApi';
 import { HiMenu } from "react-icons/hi";
 import { Link } from 'react-router-dom';
+import { FaCaretDown } from 'react-icons/fa';
 
 const catogories = categoryApi;
 
 export default function Header() {
+    const [isNavOpen, setIsNavOpen] = useState(false);
+
+    const openNav = () => {
+        setIsNavOpen(true);
+    }
+
+    const closeNav = () => {
+        setIsNavOpen(false);
+    }
+
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+
+    const toggleDropdown = () => {
+        setDropdownOpen(!dropdownOpen);
+    };
+
     return (
         <div className="header">
+            <div className={`sidenav ${isNavOpen ? 'open' : ''}`}>
+                <div className="closebtn" onClick={closeNav}>&times;</div>
+                <Link className="menu-item" to={"/"}>Home</Link>
+                <div className='menu-item category' onClick={toggleDropdown} >Category<FaCaretDown /></div>
+                <div className={`dropdown-category ${dropdownOpen ? 'open' : ''}`}>
+                    {/* <Link to={'home/furniture'} onClick={closeNav}>Furniture</Link>
+                    <Link to={'home/cosmetics'} onClick={closeNav}>Cosmetic</Link> */}
+                    {
+                        catogories.map((item, index) => {
+                            return (
+                                <Link to={item.path} key={index} onClick={closeNav}>{item.category}</Link>
+                            )
+                        })
+                    }
+                </div>
+                <Link className="menu-item" to={"/shop"} onClick={closeNav}>Shop</Link>
+                <Link className="menu-item" to={"/profile"} onClick={closeNav}>Profile </Link>
+            </div>
             <div className="container">
                 <header>
                     <Link to="/" className="logo">
@@ -23,7 +58,7 @@ export default function Header() {
                         {
                             catogories.map((item, index) => {
                                 return (
-                                    <Link to={item.path} key={index} href={item.category}>{item.category}</Link>
+                                    <Link to={item.path} key={index}>{item.category}</Link>
                                 )
                             })
                         }
@@ -46,7 +81,7 @@ export default function Header() {
                             <p>Account</p>
                         </Link>
                     </div>
-                    <div className="menu">
+                    <div onClick={openNav} className="menu">
                         <HiMenu className='icon menu' />
                     </div>
                 </header>
