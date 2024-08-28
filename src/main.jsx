18fa6472ece3fad9d62b957fee2b78a2dd26b4fb
yash-babariya <yashbabariya549@gmail.com>
2024-08-28 19:01:41 +0530

    some changes.

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
+import React from 'react';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+// import App from './App.jsx'
+const App = React.lazy(() => import('./App.jsx'))
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
