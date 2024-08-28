a58f15e0d8349161248ee14b0915b137f526277c
yash-babariya <yashbabariya549@gmail.com>
2024-08-28 18:49:52 +0530

    some changes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,15 +1,10 @@
 import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 import DefaultLayout from "./defaultLayout/defaultLayout";
-const Home = React.lazy(() => import('../components/home'));
-const Category = React.lazy(() => import('./category'));
-const Signup = React.lazy(() => import('../components/form/signup'));
-const Login = React.lazy(() => import('../components/form/login'));
-
-// import Signup from "../components/form/signup";
-// import Login from "../components/form/login";
-
-
+import Home from "../components/home";
+import Category from "./category";
+import Signup from "../components/form/signup";
+import Login from "../components/form/login";
 
 
 const router = createBrowserRouter([
