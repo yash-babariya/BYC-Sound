5909273e9c047e7b41d5ce0dd9529d701c8b551e
yash-babariya <yashbabariya549@gmail.com>
2024-09-02 14:08:15 +0530

    some changes

--- src/components/categoryPages/headphonesAndEarbuds/herobannerHAE/index.jsx
+++ src/components/categoryPages/headphonesAndEarbuds/herobannerHAE/index.jsx
@@ -1,20 +1,17 @@
 import React from 'react';
 import './herobannerHAE.scss';
-import HerobannerHAEImg from '../../../../assets/images/hero-HAE.png';
-import HerobannerMDHAEImg from '../../../../assets/images/heroMD-HAE.png';
+import { IoHeadset } from "react-icons/io5";
 
 export default function HerobannerHAE() {
     return (
         <div className='herobannerHAE'>
-            <section>
-                <div className="banner">
-                    <picture class="banner-picture">
-                        <source media="(max-width: 900px)" srcset={HerobannerMDHAEImg} />
-                        <source media="(min-width: 901px)" srcset={HerobannerHAEImg} />
-                        <img alt="null" src={HerobannerHAEImg} loading="lazy" />
-                    </picture>
-                </div>
-            </section>
+            <div className="container">
+                <section>
+                    <div className="text">
+                        <h2><IoHeadset className='icon' />Headphones & Earbuds</h2>
+                    </div>
+                </section>
+            </div>
         </div>
     )
 }
